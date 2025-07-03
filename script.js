cc7870070a62f281f58bd68a14c2e233dbe456b3
John Hankins <jrhankins883@yahoo.com>
2025-07-03 17:14:46 -0400

    Added more HTML files and a script.js file. Also added a css folder to put all the css files in

---
+++ script.js
@@ -0,0 +1,16 @@
+const findFaveButton = document.getElementById('find-fave');
+findFaveButton.onClick = () => {
+    location.href = 'findFave.html';
+};
+const randomPicksButton = document.getElementById('random-picks');
+randomPicksButton.onclick = () => {
+    location.href = 'randomPicks.html';
+}
+const slotGameButton = document.getElementById('slot-game');
+slotGameButton.onclick = () => {
+    location.href = 'slotGame.html';
+}
+const backHomeButton = document.getElementById('back-home');
+backHomeButton.onclick = () => {
+    location.href = 'index.html';
+}
